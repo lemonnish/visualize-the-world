7e16a80f1bf61079a0ef4d3606b73c086446d1bf
Lauren <lmnishizaki@gmail.com>
2018-03-09 20:35:39 -0800

    Add User#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:info] = "Account was successfully created. Welcome!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
