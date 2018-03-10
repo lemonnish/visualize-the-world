52a561f16bb3269b6f29de63f286f75a20069c5e
Lauren <lmnishizaki@gmail.com>
2018-03-09 19:33:31 -0800

    Update form destination to signup_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get '/about', to: 'static_pages#about'
-  get '/example', to: 'static_pages#example'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
+  root  'static_pages#home'
+  get   '/about',   to: 'static_pages#about'
+  get   '/example', to: 'static_pages#example'
+  get   '/contact', to: 'static_pages#contact'
+  get   '/signup',  to: 'users#new'
+  post  '/signup',  to: 'users#create'
   resource :user
 end
