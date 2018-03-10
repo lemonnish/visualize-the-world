8f65ee115349a1e69546d37c0f188c7d1bc9f7f0
Lauren <lmnishizaki@gmail.com>
2018-03-10 14:07:59 -0800

    Add test for header links for a logged-in user

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -14,6 +14,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links for logged-out user" do
     get root_path
+    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", signup_path
+  end
+
+  test "layout links for logged-in user" do
+    get root_path
+    log_in_as(users(:lauren))
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", signup_path, count: 0
   end
 end
