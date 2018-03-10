ad81ae1fb0ae10adc71499b763b194687d68d513
Lauren <lmnishizaki@gmail.com>
2018-03-09 18:51:09 -0800

    Add password validation tests (failing)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,7 +20,7 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
-  
+
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -51,5 +51,21 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "should reject non-matching passwords" do
+    @user.password = "foobar"
+    @user.password_confirmation = "foobaz"
+    assert_not @user.valid?
   end
 end
