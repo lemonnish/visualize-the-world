34afa1615bcb8f75a51d3f6ff5e7253f2a86ff1d
Lauren <lmnishizaki@gmail.com>
2018-03-09 19:42:53 -0800

    Add tests for user signup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+
+  def setup
+    @base_title = "Visualize the World"
+  end
+
+  test "should get signup" do
     get signup_path
     assert_response :success
+    assert_select "title", "Signup | #{@base_title}"
   end
 
 end
---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class SignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup" do
+    get signup_path
+    assert_template 'users/new'
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { email: "user@invalid",
+                                          password: "foo",
+                                          password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors', 6
+  end
+
+  test "valid signup" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { email: "user@example.com",
+                                          password: "password",
+                                          password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'static_pages/home'
+    assert_not flash.empty?
+  end
+end
