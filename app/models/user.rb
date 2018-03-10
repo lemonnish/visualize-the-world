c870ec0387ae751995d2f972a2bd2cf9a7b00998
Lauren <lmnishizaki@gmail.com>
2018-03-09 18:52:06 -0800

    Add email/password validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  private
+
+    # save emails as lowercase
+    def downcase_email
+      email.downcase!
+    end
 end
