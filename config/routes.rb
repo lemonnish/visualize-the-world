56874efd9eea24ec747a67e56257e5cd6d707d43
Lauren <lmnishizaki@gmail.com>
2018-03-10 23:53:38 -0800

    Limit user resource to only non-explicitly-named actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get   '/login',   to: 'sessions#new'
   post  '/login',   to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resource :user
+  resource :user,   only: [:edit, :show, :update, :destroy]
 end
