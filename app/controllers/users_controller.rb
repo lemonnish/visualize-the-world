c912c72047d3d85f8959fcda3557336d23d0f130
Lauren <lmnishizaki@gmail.com>
2018-03-10 19:29:44 -0800

    Add user show/edit views, and show/edit/update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,17 +17,50 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    old_password = params[:user][:old_password]
+    if @user.authenticate(old_password)
+      if @user.update_attributes(user_params)
+        flash[:success] = "Account successfully updated!"
+        redirect_to user_path
+      else
+        rename_password_error_messages
+        render 'edit'
+      end
+    elsif old_password.blank?
+      @user.update_attribute(:email, params[:user][:email])
+      flash[:success] = "Email successfully updated!"
+      redirect_to user_path
+    else
+      @user.errors.add(:old_password, "doesn't match the existing password")
+      render 'edit'
+    end
   end
 
   private
 
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+    # Replace occurances of "Password" with "New password"
+    def rename_password_error_messages
+      if @user.errors.messages[:password_confirmation] == ["doesn't match Password"]
+        @user.errors.messages[:password_confirmation] = ["doesn't match New password"]
+      end
+
+      password_errors = @user.errors.messages[:password]
+      if password_errors
+        @user.errors.messages[:new_password] = password_errors
+        @user.errors.delete(:password)
+      end
     end
 end
