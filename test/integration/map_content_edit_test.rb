0d3da9e0b7cda0ac786a66b33b9c04fa5e02b3a8
Lauren <lmnishizaki@gmail.com>
2018-03-12 18:47:20 -0700

    Add test skeleton for map content edit action

---
+++ test/integration/map_content_edit_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class MapContentEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:lauren)
+    @map = maps(:basic_public)
+  end
+
+  test "successful add of map content" do
+  end
+
+  test "unsuccessful add of map content" do
+  end
+
+  test "successful update of map content" do
+  end
+
+  test "unsuccessful update of map content" do
+  end
+end
