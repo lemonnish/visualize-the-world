c9349d66cdd7b30b4a6bc1f2bea2431f6c5337f8
Lauren <lmnishizaki@gmail.com>
2018-03-26 17:07:54 -0700

    Add tests for reset password mailer view, links on user login page

--- test/controllers/password_resets_controller_test.rb
+++ test/controllers/password_resets_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
-  
+
   test "should get new" do
     get new_password_reset_url
     assert_response :success
+    assert_select "title", full_title_t("Reset password")
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,6 +6,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", full_title_t("Log in")
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", new_password_reset_path
   end
 
 end
