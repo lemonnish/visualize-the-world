d99a368aa58e5a6f529baf4a508bfd2fc908813b
Lauren <lmnishizaki@gmail.com>
2018-03-26 17:01:45 -0700

    Generate PasswordResets controller, add related columns to user model

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
                              as: 'contents'
   end
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
---
+++ test/controllers/password_resets_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class PasswordResetsControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get new" do
+    get new_password_reset_url
+    assert_response :success
+  end
+end
