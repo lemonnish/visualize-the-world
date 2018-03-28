efe487b80b8da93a5281cb21e1c4074967ae2005
Lauren <lmnishizaki@gmail.com>
2018-03-27 18:28:58 -0700

    Style emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,18 +2,25 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: full_subject("Account activation")
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
+    mail to: user.email, subject: full_subject("Password reset")
   end
 
   def contact(message)
     @message = message
     mail to: ENV['PERSONAL_EMAIL'],
          from: "#{ @message.name } <#{ @message.email }>",
-         subject: "[Visualize the World] #{ @message.subject }"
+         subject: full_subject(@message.subject)
   end
+
+  private
+
+    # generate the full subject of all emails
+    def full_subject(title)
+      "[Visualize the World] #{ title }"
+    end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:lauren)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "[Visualize the World] Account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@laurennishizaki.com"], mail.from
     assert_match user.activation_token,   mail.body.encoded
@@ -16,7 +16,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:lauren)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
-    assert_equal "Password reset", mail.subject
+    assert_equal "[Visualize the World] Password reset", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@laurennishizaki.com"], mail.from
     assert_match user.reset_token,        mail.body.encoded
