7f077eee49d099ae77cdd4a5364270387d6e5afe
Lauren <lmnishizaki@gmail.com>
2018-03-27 22:28:55 -0700

    Use precompiled mailer CSS source in production

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -27,6 +27,11 @@ class UserMailer < ApplicationMailer
 
     # load the CSS
     def define_css
-      @css = Rails.application.assets["mailer.css"].to_s.html_safe
+      if Rails.env.production?
+        @css = Rails.application.assets_manifest
+                    .find_sources('mailer.css').first.to_s.html_safe
+      else
+        @css = Rails.application.assets["mailer.css"].to_s.html_safe
+      end
     end
 end
